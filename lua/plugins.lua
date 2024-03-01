827c85ba5d78262c02a3a0652981c82f574dda53
Marvista2 <90944940+Marvista2@users.noreply.github.com>
2024-03-01 06:47:39 +0100

    trouble plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -215,7 +215,17 @@ end
        
     end,
   },
-
+  
+  {
+  
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+     -- your configuration comes here
+     -- or leave it empty to use the default settings
+     -- refer to the configuration section below
+    },
+  },
   -- Python indent (follows the PEP8 style)
   { "Vimjas/vim-python-pep8-indent", ft = { "python" } },
 
