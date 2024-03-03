c1bb046a94e85f1e3731b7a31dc1c83e8fb3d798
Marvista2 <90944940+Marvista2@users.noreply.github.com>
2024-03-03 23:47:26 +0100

    Comments

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -39,3 +39,75 @@ end
 
 -- Map a key to open the Bash terminal split to the right using the directory where Neovim was started
 api.nvim_set_keymap('n', '<leader>wtw', ':lua open_bash_terminal_start_dir()<CR>', {noremap = true, silent = true})
+
+-- Function to generate organizing text in a C++ file
+-- Function to generate organizing text in a C++ file
+-- Function to generate organizing text in a C++ file
+function generate_organizing_text()
+  -- Prompt the user to input the section name
+  local section_name = vim.fn.input('Enter section name: ')
+
+  -- Define the organizing text with the provided section name
+  local organizing_text = {
+      "                ",
+      "                ",
+      "                ",
+      "//             @",
+      "//           _____",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//",
+      "//           -  -",
+      "//",
+      "//            @@",
+      "                ",
+      "                ",
+      "                "
+  }
+
+  -- Calculate the maximum length of the section name
+  local max_length = 0
+
+  -- Find the maximum length of the section name
+  for _, line in ipairs(organizing_text) do
+      local name_length = line:sub(8):find("%S")
+      if name_length then
+          max_length = math.max(max_length, #line - name_length + 1)
+      end
+  end
+
+  -- Calculate the padding for aligning the section name and @@
+  local padding = math.floor((max_length - #section_name) / 2)
+  local name_padding = string.rep(" ", padding)
+
+  -- Adjust the lines to align the section name and @@
+  organizing_text[5] = string.format("//        %s%s", name_padding, section_name)
+  -- organizing_text[7] = string.format("//             %s..", string.rep(" ", padding - 3))
+
+  -- Get the current line number
+  local current_line = vim.fn.line('.')
+
+  -- Insert each line of the organizing text above the current line
+  for _, line in ipairs(organizing_text) do
+      vim.api.nvim_buf_set_lines(0, current_line - 1, current_line - 1, true, {line})
+  end
+
+  -- Move cursor to the line below the last line of the inserted text
+  vim.cmd(tostring(current_line + #organizing_text) .. 'j')
+end
+
+
+-- Map a key to generate organizing text in a C++ file
+api.nvim_set_keymap('n', '<leader>gc', ':lua generate_organizing_text()<CR>', {noremap = true, silent = true})
