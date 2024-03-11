59064f20bdc52e3d57ead2f1d0a24c815e1b136d
Marvista2 <90944940+Marvista2@users.noreply.github.com>
2024-03-11 02:16:49 +0100

    fixed terminal openings with leader + wtw/wtd and added bottom/up/left/right

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -37,11 +37,42 @@ M.custom_mappings = {
     ["<leader>tg"] = { "<cmd>Telescope live_grep<CR>", "Telescope live_grep" },
     ["<leader>tb"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Telescope current_buffer_fuzzy_find" },
     ["<leader>ut"] = { "<cmd>UndotreeToggle<CR> | <cmd>UndotreeFocus<CR>", "Toggle undotree and focus" },
-    ["<leader>wtd"] = {
-      "<cmd>lua SetCDtoBufferCWD()<CR> | <cmd>vsplit term://%:p:h//bash<CR> | <cmd>lua SetBackPreviousCD()<CR>",
-      "Open bash terminal split in buffer dir",
+
+    ["<leader>wtdk"] = {
+      "<cmd>lua SetCDtoBufferCWD()<CR> | <cmd>aboveleft  split term://%:p:h//bash<CR> | <cmd>lua SetBackPreviousCD()<CR>",
+      "Open bash terminal split in buffer ",
     },
-    ["<leader>wtw"] = { "<cmd>lua open_bash_terminal_start_dir()<CR>", "Open Bash terminal in current directory" },
+    ["<leader>wtdj"] = {
+      "<cmd>lua SetCDtoBufferCWD()<CR> | <cmd>belowright split term://%:p:h//bash<CR> | <cmd>lua SetBackPreviousCD()<CR>",
+      "Open bash terminal split in buffer ",
+    },
+    ["<leader>wtdl"] = {
+      "<cmd>lua SetCDtoBufferCWD()<CR> | <cmd>leftabove vsplit term://%:p:h//bash<CR> | <cmd>lua SetBackPreviousCD()<CR>",
+      "Open bash terminal split in buffer ",
+    },
+    ["<leader>wtdh"] = {
+      "<cmd>lua SetCDtoBufferCWD()<CR> | <cmd>vsplit term://%:p:h//bash<CR> | <cmd>lua SetBackPreviousCD()<CR>",
+      "Open bash terminal split in buffer ",
+    },
+
+    ["<leader>wtwk"] = {
+      " <cmd>aboveleft  split term:////bash<CR> ",
+      "Open bash terminal split in buffer ",
+    },
+    ["<leader>wtwj"] = {
+      " <cmd>belowright split term:////bash<CR> ",
+      "Open bash terminal split in buffer ",
+    },
+    ["<leader>wtwl"] = {
+      " <cmd>leftabove vsplit term:////bash<CR> ",
+      "Open bash terminal split in buffer ",
+    },
+    ["<leader>wtwh"] = {
+      " <cmd>vsplit term:////bash<CR> ",
+      "Open bash terminal split in buffer ",
+    },
+      
+
     ["<leader>gc"] = { "<cmd>lua generate_organizing_text()<CR>", "Generate organizing section in C++ file" },
     ["<leader>tt"] = { "<cmd>TroubleToggle<CR>", "toggle trouble" },
 
@@ -84,14 +115,6 @@ function SetBackPreviousCD()
 
   Cwdhandler.PreviousCWD = SwitchCache
 end
-
--- Function to open a Bash terminal split to the right using the directory where Neovim was started
-function open_bash_terminal_start_dir()
-  local start_dir = vim.fn.getcwd()
-  start_dir = start_dir:gsub("/", "\\")
-  vim.cmd("vsplit | terminal bash -c \"cd '" .. start_dir .. "' && exec $SHELL\"")
-end
-
 -- Function to generate organizing text in a C++ file
 function generate_organizing_text()
   local section_name = "@s." .. vim.fn.input "Enter section name: "
