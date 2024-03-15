3b98f646530f7a4d5a34f8bb5723e8a380b959ac
Marvista2 <90944940+Marvista2@users.noreply.github.com>
2024-03-15 06:07:51 +0100

    fixed lspconfig bug

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" , "pyright" }
+local servers = { "html", "cssls", "tsserver",  "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,5 +13,15 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+local cmp_nvim_lsp = require "cmp_nvim_lsp"
+
+require("lspconfig").clangd.setup {
+  on_attach = on_attach,
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+}
 -- 
 -- lspconfig.pyright.setup { blabla}
