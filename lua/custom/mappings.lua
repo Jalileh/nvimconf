17aaf8b1a442d8033b077a5611e8f1fd20fbe7fc
Jalileh <fayardwatson@outlook.com>
2024-11-29 23:09:42 +0100

    lsp fix

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -17,19 +17,7 @@ M.general = {
     [">"] = { ">gv", "indent" },
   },
 }
-M.dap = {
-  plugin = true,
-  n = {
-    ["<leader>db"] = {
-      "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line",
-    },
-    ["<leader>dr"] = {
-      "<cmd> DapContinue <CR>",
-      "Start or continue the debugger",
-    }
-  }
-}
+ 
 -- Define your custom mappings
 M.custom_mappings = {
   n = {
@@ -81,65 +69,134 @@ M.custom_mappings = {
     -- switchback to last cwd
     ["<leader>wcp"] = { "<cmd>lua SetBackPreviousCD()<CR>", "Point Neovim to Previous Cached CWD" },
   },
+
+ 
+
+  -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
+
+  n = {
+    ["gD"] = {
+      function()
+        vim.lsp.buf.declaration()
+      end,
+      "LSP declaration",
+    },
+
+    ["gd"] = {
+      function()
+        vim.lsp.buf.definition()
+      end,
+      "LSP definition",
+    },
+
+    ["K"] = {
+      function()
+        vim.lsp.buf.hover()
+      end,
+      "LSP hover",
+    },
+
+    ["gi"] = {
+      function()
+        vim.lsp.buf.implementation()
+      end,
+      "LSP implementation",
+    },
+
+    ["<leader>ls"] = {
+      function()
+        vim.lsp.buf.signature_help()
+      end,
+      "LSP signature help",
+    },
+
+    ["<leader>D"] = {
+      function()
+        vim.lsp.buf.type_definition()
+      end,
+      "LSP definition type",
+    },
+
+    ["<leader>ra"] = {
+      function()
+        require("nvchad.renamer").open()
+      end,
+      "LSP rename",
+    },
+
+    ["<leader>ca"] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      "LSP code action",
+    },
+
+    ["gr"] = {
+      function()
+        vim.lsp.buf.references()
+      end,
+      "LSP references",
+    },
+
+    ["<leader>lf"] = {
+      function()
+        vim.diagnostic.open_float { border = "rounded" }
+      end,
+      "Floating diagnostic",
+    },
+
+    ["[d"] = {
+      function()
+        vim.diagnostic.goto_prev { float = { border = "rounded" } }
+      end,
+      "Goto prev",
+    },
+
+    ["]d"] = {
+      function()
+        vim.diagnostic.goto_next { float = { border = "rounded" } }
+      end,
+      "Goto next",
+    },
+
+    ["<leader>q"] = {
+      function()
+        vim.diagnostic.setloclist()
+      end,
+      "Diagnostic setloclist",
+    },
+
+    ["<leader>wa"] = {
+      function()
+        vim.lsp.buf.add_workspace_folder()
+      end,
+      "Add workspace folder",
+    },
+
+    ["<leader>wr"] = {
+      function()
+        vim.lsp.buf.remove_workspace_folder()
+      end,
+      "Remove workspace folder",
+    },
+
+    ["<leader>wl"] = {
+      function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end,
+      "List workspace folders",
+    },
+  },
+
+  v = {
+    ["<leader>ca"] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      "LSP code action",
+    },
+  },
+
 }
-
--- very insecure lua code, ive yet formed any neurons to write any good algorithm or code
-Cwdhandler = {}
-Cwdhandler.PreviousCWD = vim.fn.getcwd()
-Cwdhandler.Status = 0
--- enter current directory and set its focus
-function SetCDtoBufferCWD()
-  local buffercwd = vim.fn.expand "%:p:h"
-  print("Neovim CWD pointed to: " .. vim.fn.expand "%:p:h")
-
-  Cwdhandler.PreviousCWD = vim.fn.getcwd()
-
-  if Cwdhandler.Status == 0 then
-    Cwdhandler.Status = 1
-  elseif Cwdhandler.Status == 1 then
-    Cwdhandler.Status = 0
-  else
-    Cwdhandler.Status = Cwdhandler.Status + 1
-  end
-
-  vim.cmd("cd " .. buffercwd)
-end
-
-
-
-SwitchCache = "unset"
-function SetBackPreviousCD()
-  print(Cwdhandler.PreviousCWD)
-
-  SwitchCache = vim.fn.getcwd()
-
-  vim.cmd("cd " .. Cwdhandler.PreviousCWD)
-
-  Cwdhandler.PreviousCWD = SwitchCache
-end
--- Function to generate organizing text in a C++ file
-function generate_organizing_text()
-  local section_name = "@s." .. vim.fn.input "Enter section name: "
-
-  local organizing_text = {
-    " ",
-    "////       ",
-    "////       ",
-    "////       ",
-    "////       ",
-    "////  " .. section_name,
-    "////",
-    string.rep("/", 70),
-    string.rep("/", 70),
-    " ",
-  }
-
-  local current_line = vim.fn.line "."
-  for _, line in ipairs(organizing_text) do
-    vim.api.nvim_buf_set_lines(0, current_line - 1, current_line - 1, true, { line })
-  end
-
-  vim.cmd(tostring(current_line + #organizing_text) .. "j")
-end
-
+ 
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,67 +2,60 @@ local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
-   -- {
-   --       event = "VeryLazy",
-   --    "dense-analysis/ale",
-   --    config = function()
-   --       -- Configuration goes here.
-   --       local g = vim.g
-   --       local b = vim.b
-   --
-   --       -- " Only run linters named in ale_linters settings."
-   --       g.ale_linters_explicit = 1
-   --
-   --       -- Disable all linters
-   --
-   --       -- Enable specific linters for desired file types
-   --
-   --       vim.g.ale_linters = {
-   --          sh = { "bashate", "cspell", "language_server", "shell", "shellcheck" },
-   --          bash = { "bashate", "cspell", "language_server", "shell", "shellcheck" },
-   --          cpp = { "clangtidy" },
-   --       }
-   --
-   --       g.ale_lint_on_text_changed = "never"
-   --       g.ale_lint_on_insert_leave = 1
-   --       g.ale_lint_on_enter = 0
-   --    end,
-   -- },
-
-   -- {
-   --    event = "VeryLazy",
-   --    "mfussenegger/nvim-dap",
-   --    config = function()
-   --       require("nvim-web-devicons").get_icons()
-   --    end,
-   -- },
-   --
-   -- {
-   --    "folke/trouble.nvim",
-   --    dependencies = { "nvim-tree/nvim-web-devicons" },
-   --    lazy = false,
-   -- },
-   --
-   -- {
-   --    "ray-x/lsp_signature.nvim",
-   --    event = "VeryLazy",
-   --    opts = {},
-   --    config = function(_, opts)
-   --       require("lsp_signature").setup(opts)
-   --    end,
-   -- },
-   --
-   -- nvchad edits below
-   --
-   -- {
-   --    "neovim/nvim-lspconfig",
-   --    config = function()
-   --       require "plugins.configs.lspconfig"
-   --       require "custom.configs.lspconfig"
-   --    end, -- Override to setup mason-lspconfig
-   -- },
-   --
-   -- override plugin configs
+   {
+         event = "VeryLazy",
+      "dense-analysis/ale",
+      config = function()
+         -- Configuration goes here.
+         local g = vim.g
+         local b = vim.b
+   
+         -- " Only run linters named in ale_linters settings."
+         g.ale_linters_explicit = 1
+   
+         -- Disable all linters
+   
+         -- Enable specific linters for desired file types
+   
+         vim.g.ale_linters = {
+            sh = { "bashate", "cspell", "language_server", "shell", "shellcheck" },
+            bash = { "bashate", "cspell", "language_server", "shell", "shellcheck" },
+            cpp = { "clangtidy" },
+         }
+   
+         g.ale_lint_on_text_changed = "never"
+         g.ale_lint_on_insert_leave = 1
+         g.ale_lint_on_enter = 0
+      end,
+   },
+ 
+   
+   {
+      "folke/trouble.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      lazy = false,
+   },
+   
+   {
+      "ray-x/lsp_signature.nvim",
+      event = "VeryLazy",
+      opts = {},
+      config = function(_, opts)
+         require("lsp_signature").setup(opts)
+      end,
+   },
+   
+ 
+   
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         require "plugins.configs.lspconfig"
+         require "custom.configs.lspconfig"
+      end, -- Override to setup mason-lspconfig
+   },
+   
+ 
    {
       "williamboman/mason.nvim",
       opts = overrides.mason,
