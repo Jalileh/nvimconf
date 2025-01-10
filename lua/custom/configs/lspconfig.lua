99f431e725fdf95537157af6838f4a62e21a5ed8
Jalileh <yacoub.m94f@gmail.com>
2025-01-11 00:10:04 +0100

    markdown added why not?

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -6,88 +6,107 @@ local lspconfig = require "lspconfig"
 
 local cmp_nvim_lsp = require "cmp_nvim_lsp"
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "gopls" }
+local servers = { "markdown_oxide", "html", "cssls", "ts_ls", "gopls" }
 for _, lsp in ipairs(servers) do
-   lspconfig[lsp].setup {
-      on_attach = on_attach, -- needed for lsp keybinds
-      capabilities = capabilities,
-   }
+	lspconfig[lsp].setup {
+		on_attach = on_attach, -- needed for lsp keybinds
+		capabilities = capabilities,
+	}
 end
+-- An example nvim-lspconfig capabilities setting
+
+
+lspconfig.markdown_oxide.setup({
+	-- Ensure that dynamicRegistration is enabled! This allows the LS to take into account actions like the
+	-- Create Unresolved File code action, resolving completions for unindexed code blocks, ...
+	capabilities = vim.tbl_deep_extend(
+		'force',
+		capabilities,
+		{
+			workspace = {
+				didChangeWatchedFiles = {
+					dynamicRegistration = true,
+				},
+			},
+		}
+	),
+	on_attach = on_attach -- configure your on attach config
+})
 
 lspconfig.lua_ls.setup {
-   capabilities = capabilities,
-   on_attach = on_attach,
-   settings = {
-      Lua = {
-         format = {
-            enable = true,
-            -- Put format options here
-            -- NOTE: the value should be STRING!!
-            defaultConfig = {
-               indent_style = "tab",
-               indent_size = "3",
-            },
-         },
-      },
-   },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			format = {
+				enable = true,
+				-- Put format options here
+				-- NOTE: the value should be STRING!!
+				defaultConfig = {
+					indent_style = "tab",
+					indent_size = "3",
+				},
+			},
+		},
+	},
 }
 lspconfig.pyright.setup {
-   capabilities = capabilities,
-   on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 
 }
 
 
 lspconfig.clangd.setup {
-   on_attach = function(client, bufnr)
-      print "clangd attached" -- Move print statement inside the on_attach function
+	on_attach = function(client, bufnr)
+		print "clangd attached" -- Move print statement inside the on_attach function
 
-      -- Your custom on_attach function here
-      -- You can define custom key mappings, highlight settings, etc.
+		-- Your custom on_attach function here
+		-- You can define custom key mappings, highlight settings, etc.
 
-      -- Merge capabilities
-      client.server_capabilities.document_formatting = true
-      client.server_capabilities.document_range_formatting = true
+		-- Merge capabilities
+		client.server_capabilities.document_formatting = true
+		client.server_capabilities.document_range_formatting = true
 
-      -- retarded ai dosent even make this fucking do it
-      client.config.cmd = {
-         "clangd",
-         "--background-index",
-         "--completion-style=detailed",
-         "--function-arg-placeholders",
-         "-j4",
-         "--fallback-style=llvm",
-         "--header-insertion=never",
-      }
-      client.config.root_dir = lspconfig.util.root_pattern(".git", "compile_commands.json", "CMakeLists.txt")
-      client.config.settings = {
-         clangd = {
-            compileCommandsDirectory = "build",
-            indexer = {
-               threads = 4,
-               background = true,
-               trace = "verbose",
-               pchStorage = "memory",
-            },
-         },
-      }
-      client.config.init_options = {
-         usePlaceholders = true,
-         completeUnimported = true,
-         clangdFileStatus = true,
-      }
-   end,
-   capabilities = cmp_nvim_lsp.default_capabilities(),
+		-- retarded ai dosent even make this fucking do it
+		client.config.cmd = {
+			"clangd",
+			"--background-index",
+			"--completion-style=detailed",
+			"--function-arg-placeholders",
+			"-j4",
+			"--fallback-style=llvm",
+			"--header-insertion=never",
+		}
+		client.config.root_dir = lspconfig.util.root_pattern(".git", "compile_commands.json", "CMakeLists.txt")
+		client.config.settings = {
+			clangd = {
+				compileCommandsDirectory = "build",
+				indexer = {
+					threads = 4,
+					background = true,
+					trace = "verbose",
+					pchStorage = "memory",
+				},
+			},
+		}
+		client.config.init_options = {
+			usePlaceholders = true,
+			completeUnimported = true,
+			clangdFileStatus = true,
+		}
+	end,
+	capabilities = cmp_nvim_lsp.default_capabilities(),
 
-   cmd = {
-      "clangd",
-      "--background-index",
-      "--completion-style=detailed",
-      "--function-arg-placeholders=1",
-      "--offset-encoding=utf-16",
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--completion-style=detailed",
+		"--function-arg-placeholders=1",
+		"--offset-encoding=utf-16",
 
-      "--header-insertion=never",
-   },
+		"--header-insertion=never",
+	},
 }
 
 -- local clangd_config = {
