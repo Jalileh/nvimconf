62959d2aa3d6493f4514c5707eac0b507a522199
Jalileh <yacoub.m94f@gmail.com>
2025-03-14 14:12:57 +0100

    Added copilot as default provider for the avante plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -28,14 +28,25 @@ local plugins = {
 			g.ale_lint_on_enter = 1
 		end,
 	},
-
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		lazy = false,
 		version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
 		opts = {
-			-- add any opts here
+			provider = "copilot",
+			auto_suggestions_provider = "copilot",
+			behaviour = {
+				auto_suggestions = true,
+			},
 		},
 		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 		build = "make",
@@ -48,12 +59,15 @@ local plugins = {
 			"hrsh7th/nvim-cmp",      -- autocompletion for avante commands and mentions
 			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 			"zbirenbaum/copilot.lua", -- for providers='copilot'
+
 			{
 				-- support for image pasting
 				"HakonHarnes/img-clip.nvim",
 				event = "VeryLazy",
 				opts = {
 					-- recommended settings
+
+
 					default = {
 						embed_image_as_base64 = false,
 						prompt_for_file_name = false,
